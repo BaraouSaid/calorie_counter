cf0cb827ac6aa6607285213ecfeba97e15c25755
Said <slaoualy@yahoo.fr>
2024-02-18 14:19:27 +0100

    feat: Stored budgetCalories and checked for truthiness of isError for the function logic to excute or simply return

--- js/script.js
+++ js/script.js
@@ -33,6 +33,9 @@ function addEntry() {
 function calculateCalories(e) {
   e.preventDefault();
   isError = false;
+  if (isError) {
+    return;
+  }
   const breakfastNumberInputs = document.querySelectorAll(
     '#breakfast input[type=number]'
   );
@@ -54,6 +57,7 @@ function calculateCalories(e) {
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
 }
 
 function getCaloriesFromInputs(list) {
