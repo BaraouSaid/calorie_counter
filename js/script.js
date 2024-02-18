a492e5de27f1cacc588a8c241831df14491a7c69
Said <slaoualy@yahoo.fr>
2024-02-18 14:12:59 +0100

    feat: created variables calling getCaloriesFromInputs to get the value of all form fields

--- js/script.js
+++ js/script.js
@@ -50,6 +50,10 @@ function calculateCalories(e) {
   );
 
   const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
 }
 
 function getCaloriesFromInputs(list) {
